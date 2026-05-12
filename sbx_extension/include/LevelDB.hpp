afb1405aafc69664daef6d6cfca5630d90c2f585
blueloveTH <blueloveTH@foxmail.com>
2026-05-12 14:23:02 +0800

    mv sbx_ext

--- sbx_extension/include/LevelDB.hpp
+++
@@ -1,5 +0,0 @@
-namespace pkpy {
-
-void setup_leveldb_module();
-
-}   // namespace pkpy
--- sbx_extension/include/sbx.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace pkpy {
-
-void setup_sbx_godot_classes();
-void setup_sbx_python_modules();
-
-} // namespace pkpy
