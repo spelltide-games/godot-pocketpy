344134602f0fce0f4e6ae86d8b38b5f26e478de2
blueloveTH <blueloveTH@foxmail.com>
2026-06-14 10:36:31 +0800

    Update LockstepGoClientDebug.gd

--- demo/sbx_extension/LockstepGoClientDebug.gd
+++ demo/sbx_extension/LockstepGoClientDebug.gd
@@ -2,14 +2,11 @@ extends LockstepGoClient
 
 class_name LockstepGoClientDebug
 
-@export var enabled: bool = true
 @export var host: String = '127.0.0.1'
 @export var port: int = 5999
 @export var udp_redundancy: int = 1
 
 @export var is_host_player: bool
-
-static var p1_id: String
 
 var server_latency_wnd = TimeDeltaWindow.new(20, 0)
 var local_latency_wnd = TimeDeltaWindow.new(20, 0)
@@ -22,10 +19,16 @@ func check_error(err: String) -> bool:
 	print(err)
 	return false
 
+func sync_game_state(curr_room: Dictionary):
+	var curr_players: Dictionary = curr_room['players']
+	var first_id = curr_players.values().get(0)['id']
+	var sync_frame_id = curr_room['frame_id']
+	var state = await export_game_state(first_id, sync_frame_id)
+	print('game_state: ', state)
+	await get_tree().create_timer(1.0).timeout
+	apply_game_state(state)
+
 func _ready():
-	if not enabled:
-		return
-		
 	await connect_ws(host, port)
 	print('ws connected: %s' % id)
 	
@@ -34,19 +37,12 @@ func _ready():
 		var res = await create_room('1.0', 4, 20)
 		if check_error(res['error']):
 			new_room = res['retval']
-			p1_id = id
 	else:
 		while true:
 			# pre-join room
 			var res = await join_room(port+1, '1.0', true)
 			if check_error(res['error']):
-				var curr_players: Dictionary = res['retval']['players']
-				var first_id = curr_players.values().get(0)['id']
-				var sync_frame_id = res['retval']['frame_id']
-				var state = await export_game_state(first_id, sync_frame_id)
-				print('game_state: ', state)
-				await get_tree().create_timer(1.0).timeout
-				apply_game_state(state)
+				sync_game_state(res['retval'])
 				# join room
 				res = await join_room(port+1, '1.0', false)
 				if check_error(res['error']):
@@ -93,47 +89,42 @@ func _on_room_event(event: Dictionary) -> void:
 		
 
 func _process(delta: float) -> void:
-	if not enabled:
-		return
-		
 	poll(delta)
 	while get_available_frame_count() > 0:
-		var server_frame = get_frame()
+		var frame = get_frame()
 		assert(stabilizer != null)
 		server_latency_wnd.tick()
-		stabilizer.add_frame(server_frame)
-		var frame_id = server_frame['frame_id']
-		assert(room['frame_id'] == frame_id)
+		stabilizer.add_frame(frame)
+		assert(room['frame_id'] == frame['frame_id'])
 		room['frame_id'] += 1
-		
-		if is_host_player:
-			print(server_frame['frame_id'], server_frame['inputs'])
-		#if !is_host_player:
-			#print('P2: ', frame_id, ' | ', $P1/HSlider.value)
-		#else:
-			#print('P1: ', frame_id, ' | ', $P1/HSlider.value)
 		
 	if stabilizer != null:
 		var frame = stabilizer.get_frame(delta)
 		if frame != null:
-			local_latency_wnd.tick()	
-			var inputs: Dictionary = frame['inputs']
-			for conv in inputs.keys():
-				var d = inputs[conv][0]
-				for k in d.keys():
-					get_node(k).get_child(0).value += d[k]
-			simulated_frame_id = frame['frame_id']
-			
-		if is_host_player:
-			if Input.is_key_pressed(KEY_A):
-				send_input({self.name: -1})
-			if Input.is_key_pressed(KEY_D):
-				send_input({self.name: 1})
-		else:
-			if Input.is_key_pressed(KEY_LEFT):
-				send_input({self.name: -1})
-			if Input.is_key_pressed(KEY_RIGHT):
-				send_input({self.name: 1})
+			simulate(frame)
+			gather_inputs()
+
+
+func simulate(frame: Dictionary):
+		local_latency_wnd.tick()	
+		var inputs: Dictionary = frame['inputs']
+		for conv in inputs.keys():
+			var d = inputs[conv][0]
+			for k in d.keys():
+				get_node(k).get_child(0).value += d[k]
+		simulated_frame_id = frame['frame_id']
+				
+func gather_inputs():
+	if is_host_player:
+		if Input.is_key_pressed(KEY_A):
+			send_input({self.name: -1})
+		if Input.is_key_pressed(KEY_D):
+			send_input({self.name: 1})
+	else:
+		if Input.is_key_pressed(KEY_LEFT):
+			send_input({self.name: -1})
+		if Input.is_key_pressed(KEY_RIGHT):
+			send_input({self.name: 1})
 
 func print_latency():
 	while true:
