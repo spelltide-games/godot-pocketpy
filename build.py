3190ac979c9dc2b4c751af706a714452ab6a44b2
blueloveTH <blueloveth@foxmail.com>
2026-09-05 23:14:51 +0800

    1

--- build.py
+++ build.py
@@ -5,8 +5,7 @@ import subprocess
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', type=str, default='Debug')
 parser.add_argument('--platform', type=str, default='win32')
-parser.add_argument('--cmake-arg', action='append', default=[], metavar='ARG',
-                    help='Extra CMake configure argument; repeat --cmake-arg=-DNAME=VALUE as needed')
+parser.add_argument('--with-sbx', action='store_true', help='Build the SBX extension')
 
 def run(cmd: list[str]):
     print(subprocess.list2cmdline(cmd), flush=True)
@@ -16,7 +15,7 @@ args = parser.parse_args()
 config: str = args.config
 platform: str = args.platform
 
-extra_flags = []
+extra_flags = [f'-DGODOT_POCKETPY_WITH_SBX={"ON" if args.with_sbx else "OFF"}']
 
 if platform == 'android':
     toolchain_file = os.path.join(os.environ['ANDROID_NDK_HOME'], 'build/cmake/android.toolchain.cmake')
@@ -33,5 +32,5 @@ elif platform == 'ios':
 if config == 'Release':
     extra_flags.append('-DGODOTCPP_TARGET=template_release')
 
-run(['cmake', '-B', 'build', f'-DCMAKE_BUILD_TYPE={config}', *extra_flags, *args.cmake_arg])
+run(['cmake', '-B', 'build', f'-DCMAKE_BUILD_TYPE={config}', *extra_flags])
 run(['cmake', '--build', 'build', '--config', config])
