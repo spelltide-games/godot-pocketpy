ae3d620747f973831d4a72f16689f2702154b9f0
blueloveTH <blueloveth@foxmail.com>
2026-09-05 14:25:37 +0800

    1

--- demo/bugs/prefabs/LeakNode.py
+++ demo/bugs/prefabs/LeakNode.py
@@ -1,6 +1,11 @@
 from godot import *
 from godot.classes import Node2D
 
+from gc import setup_debug_callback
+
+
+setup_debug_callback(lambda phase, info: print(f"GC phase: {phase}\n{info}"))
+
 count = 0
 
 class LeakNode(Extends(Node2D)):
@@ -14,4 +19,13 @@ class LeakNode(Extends(Node2D)):
         count += 1
         #print(count)
 
-        self.leak = [1] * 1000
+        if 1:
+            # this is okay
+            # pkpy will run gc to collect them
+            self.leak = [object() for i in range(1000)]
+        else:
+            # this is not okay
+            # pkpy's gc is based on object counts
+            # allocating large (but few) objects may not trigger gc
+            # so you may think there is leakage
+            self.leak = [1] * 1000
